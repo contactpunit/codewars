c4d6b02f954db893dec1043d45e4caa5ee2f6f6c
contactpunit <contactpunitjain@gmail.com>
2024-12-12 12:30:23 +0530

    concize solution

--- Easy/Array/EvenTimesLast.ts
+++ Easy/Array/EvenTimesLast.ts
@@ -6,14 +6,5 @@
 // If the sequence is empty, you should return 0.
 
 export function evenLast(numbers) {
-    let total = 0
-    for(let i = 0; i < numbers.length; i++) 
-    {
-        if(typeof numbers[i] === 'number' && 
-        !isNaN(numbers[i]) && 
-        i % 2 === 0) {
-            total += numbers[i]
-        }
-    }
-    return numbers.length > 0 ? total * numbers[numbers.length - 1] : 0
+    return numbers.filter((e, idx) => idx % 2 === 0).reduce((a, b) => a + b, 0) * numbers[numbers.length - 1] || 0
 }
