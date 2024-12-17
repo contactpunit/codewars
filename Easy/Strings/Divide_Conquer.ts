d854e67ecb1e19e7994e6a3308a03ec566a9d806
contactpunit <contactpunitjain@gmail.com>
2024-12-17 16:28:56 +0530

    easier way

--- Easy/Strings/Divide_Conquer.ts
+++ Easy/Strings/Divide_Conquer.ts
@@ -3,16 +3,6 @@
 
 // Return as a number.
 
-
 export function divCon(x){
-    const result = x.reduce((acc, curr) => {
-        if(typeof curr === 'string') {
-            acc[0] += +curr
-            return acc
-        } else {
-            acc[1] += curr
-            return acc
-        }
-    }, [0, 0])
-    return result[1] - result[0]
+    return x.reduce((acc, curr) => typeof curr === 'number' ? acc += curr : acc -= +curr, 0)
 }
