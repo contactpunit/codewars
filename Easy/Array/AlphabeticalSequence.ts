33dc76f03aad476cbe89eba7125d93960fb56c99
contactpunit <contactpunitjain@gmail.com>
2024-12-25 13:32:20 +0530

    alpha seq

---
+++ Easy/Array/AlphabeticalSequence.ts
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// In this kata you will be given a random string of letters and tasked with returning them as a string of comma-separated sequences sorted alphabetically, with each sequence starting with an uppercase character followed by n-1 lowercase characters, where n is the letter's alphabet position 1-26.
+
+// Example
+// alphaSeq("ZpglnRxqenU") -> "Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Xxxxxxxxxxxxxxxxxxxxxxxx,Zzzzzzzzzzzzzzzzzzzzzzzzzz"
+// Technical Details
+// The string will include only letters.
+// The first letter of each sequence is uppercase followed by n-1 lowercase.
+// Each sequence is separated with a comma.
+// Return value needs to be a string.
+
+export function alphaSeq (str) {
+    const res = str.split('').map(c => c.toLowerCase()).sort((a,b) => a.localeCompare(b))
+    return res.map(e => e.toUpperCase().concat(e.repeat(e.charCodeAt(0) - 97))).join(',')
+}
---
+++ test/Easy/Array/AlphabeticalSequence.test.ts
@@ -0,0 +1,13 @@
+import {it, describe, expect} from 'vitest'
+import { alphaSeq } from '../../../Easy/Array/AlphabeticalSequence'
+
+describe('alphaSeq test suite()', () => {
+    it.each([
+        ["ZpglnRxqenU", "Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Xxxxxxxxxxxxxxxxxxxxxxxx,Zzzzzzzzzzzzzzzzzzzzzzzzzz"],
+        ["NyffsGeyylB", "Bb,Eeeee,Ffffff,Ffffff,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Sssssssssssssssssss,Yyyyyyyyyyyyyyyyyyyyyyyyy,Yyyyyyyyyyyyyyyyyyyyyyyyy,Yyyyyyyyyyyyyyyyyyyyyyyyy"],
+        ["EvidjUnokmM", "Dddd,Eeeee,Iiiiiiiii,Jjjjjjjjjj,Kkkkkkkkkkk,Mmmmmmmmmmmmm,Mmmmmmmmmmmmm,Nnnnnnnnnnnnnn,Ooooooooooooooo,Uuuuuuuuuuuuuuuuuuuuu,Vvvvvvvvvvvvvvvvvvvvvv"],
+        ["HbideVbxncC", "Bb,Bb,Ccc,Ccc,Dddd,Eeeee,Hhhhhhhh,Iiiiiiiii,Nnnnnnnnnnnnnn,Vvvvvvvvvvvvvvvvvvvvvv,Xxxxxxxxxxxxxxxxxxxxxxxx"]
+    ])('%s input returns %s', (input, output) => {
+        expect(alphaSeq(input)).toBe(output)
+    })
+})
