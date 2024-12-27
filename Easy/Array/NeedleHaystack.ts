edc9ac46c9854954549c673df961f145efa629f1
contactpunit <contactpunitjain@gmail.com>
2024-12-27 19:26:44 +0530

    needle haystack

---
+++ Easy/Array/NeedleHaystack.ts
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// Example(Input --> Output)
+
+// ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+
+export function findNeedle(haystack) {
+    const idx = haystack.findIndex(e => e === 'needle')
+    if (idx != -1 ) return `found the needle at position ${idx}`
+}
