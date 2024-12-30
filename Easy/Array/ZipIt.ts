fb763c5bd07cfc5d9053d90173a53eb74df14998
contactpunit <contactpunitjain@gmail.com>
2024-12-30 13:41:32 +0530

    nice problem using this.

---
+++ Easy/Array/ZipIt.ts
@@ -0,0 +1,25 @@
+// DESCRIPTION:
+// Write
+
+// Array.prototype.zip = function (arr, fn) {}
+// that merges the corresponding elements of two sequences using a specified selector function fn (a block in Ruby)
+
+// For example:
+
+// var a = [1, 2, 3, 4, 5];
+// var b = ['a','b'];
+// a.zip(b, (a, b) => a + b) === ['1a', '2b']
+
+// var a = [1, 2, 3, 4, 5];
+// var b = ['a','b','c','d','e'];
+// a.zip(b, (a, b) => a + b.charCodeAt(0)) === [98, 100, 102, 104, 106]
+// if arrays have different lengths, go up to the minimum length and then stop.
+
+export function zip(arr, fn) {
+    const slength = this.length < arr.length ? this.length : arr.length
+    const result: any = []
+    for(let i = 0; i < slength; i ++) {
+        result.push(fn(this[i], arr[i]))
+    }
+    return result
+}
