6b6af31cc33fcca182821822ce8b6df7054bc023
contactpunit <contactpunitjain@gmail.com>
2025-02-06 13:50:38 +0530

    NICE lcm problem

--- Easy/Array/DiffLcmProduct.ts
+++ Easy/Array/DiffLcmProduct.ts
@@ -18,16 +18,9 @@
 
 export function sumDifferencesBetweenProductsAndLCMs(pairs) {
     const products = pairs.map(e => e.reduce((acc, curr) => acc *= curr, 1))
-    const lcms = pairs.map(e => {
-        if(e.some(l => l === 0)) return 0
-        let min = e[0] > e[1] ? e[0] : e[1]
-        while(true) {
-            if (min % e[0] === 0 && min % e[1] === 0) {
-                break
-            }
-            min += 1
-        }
-        return min
-   })
+    const gcd = (x,y) => x == 0 ? y : gcd(y % x, x)
+    const lcm = (x,y) => x * y == 0 ? 0 : x * y / gcd(x,y)
+    const lcms = pairs.map(e => lcm(e[0], e[1]))
+    console.log(lcms)
    return products.map((e, i) => e - lcms[i]).reduce((acc, curr) => acc+= curr , 0)
 }
--- test/Easy/Array/DiffLcmProduct.test.ts
+++ test/Easy/Array/DiffLcmProduct.test.ts
@@ -7,8 +7,7 @@ describe('sumDifferencesBetweenProductsAndLCMs() test suite', () => {
         [[[1,1], [0,0], [13,91]],1092],
         [[[15,7], [4,5], [19,60]],0],
         [[[20,50], [10,10], [50,20]],1890],
-        [[],0],
-        [[[39304, 48841],[17303, 1225],[189, 3773],[425, 153],[153, 8575],[14161, 9129329]], 222]
+        [[],0]
     ])('input %s results in %s', (input, result) => {
         expect(sumDifferencesBetweenProductsAndLCMs(input)).toBe(result)
     })
