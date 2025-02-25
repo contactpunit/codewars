932ebd2b07fa9e8be919d6a730675a90c3d50c85
Punit Jain <contactpunitjain@gmail.com>
2025-02-25 11:15:27 +0530

    llist to array

---
+++ test/Easy/Array/LinkedList2Array.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest'
+import { listToArray } from '../../../Easy/Array/LinkedList2Array'
+
+describe('listToArray test suite', () => {
+    const list1 = { value: 1, next: { value: 2, next: { value: 3, next: null } } };
+    const list2 = { value: "foo", next: { value: "bar", next: null } };
+
+    it.each([
+        [list1, [1, 2, 3]],
+        [list2, ["foo", "bar"]]
+    ])('input %s gets result %s', (input, result) => {
+        expect(listToArray(input)).toBe(result)
+    })
+})
